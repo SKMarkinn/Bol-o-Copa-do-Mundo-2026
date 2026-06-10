74eb32ef907641429edf962a492f27c3e68002c8
SKMarkinn <limamarcosemanuel479@gmail.com>
2026-06-09 23:44:04 -0300

    Update app.py

--- app.py
+++ app.py
@@ -5,8 +5,7 @@ from firebase_admin import credentials, db
 if not firebase_admin._apps:
     cred = credentials.Certificate(st.secrets["gcp_service_account"])
     firebase_admin.initialize_app(cred, {
-        'databaseURL': 'https://bolao-copa-do-mundo-2026-c4d2c-default-rtdb.firebaseio.com/'
-    })
+        'databaseURL':'https://bolao-copa-do-mundo-2026-c4d2c-default-rtdb.firebaseio.com/' })
 import streamlit as st
 import pandas as pd
 from datetime import datetime, timedelta
