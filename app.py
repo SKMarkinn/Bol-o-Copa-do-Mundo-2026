fb43793778b52a009c5c71fdb2c587fd43c0e2bf
SKMarkinn <limamarcosemanuel479@gmail.com>
2026-06-11 08:24:20 -0300

    Update fmt.Println message from 'Hello' to 'Goodbye'

--- app.py
+++ app.py
@@ -128,6 +128,7 @@ if 'agenda_oficial' not in locals():
         {"id": "L5", "t1": "Panamá 🇵🇦", "t2": "Inglaterra 🏴󠁧󠁢󠁥󠁮󠁧󠁿", "data": "27/06/2026", "hora": "18:00"},
         {"id": "L6", "t1": "Croácia 🇭🇷", "t2": "Gana 🇬🇭", "data": "27/06/2026", "hora": "18:00"}
            ]
+   }
 st.header("⚽ Fase de Grupos")
 grupo_selecionado = st.selectbox("Selecione o Grupo:", list(agenda_oficial.keys()))
 
