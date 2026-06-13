6f67db0bde34d06929050e8071103ad022471aa9
SKMarkinn <limamarcosemanuel479@gmail.com>
2026-06-13 16:40:35 -0300

    Update app.py

--- app.py
+++ app.py
@@ -32,25 +32,25 @@ with st.sidebar:
         if st.button("Sair"):
             st.session_state.nick = None
             st.rerun()
-if not firebase_admin._apps:
+#if not firebase_admin._apps:
     # Monta o dicionário de credenciais a partir das variáveis do Render
-    cred_dict = {
-        "type": "service_account",
-        "project_id": os.environ.get("FIREBASE_PROJECT_ID"),
-        "private_key_id": os.environ.get("FIREBASE_PRIVATE_KEY_ID"),
-        "private_key": os.environ.get("FIREBASE_PRIVATE_KEY").replace('\\n', '\n'),
-        "client_email": os.environ.get("FIREBASE_CLIENT_EMAIL"),
-        "client_id": os.environ.get("FIREBASE_CLIENT_ID"),
-        "auth_uri": "https://accounts.google.com/o/oauth2/auth",
-        "token_uri": "https://oauth2.googleapis.com/token",
-        "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
-        "client_x509_cert_url": os.environ.get("FIREBASE_CLIENT_CERT_URL")
-    }
+ #   cred_dict = {
+  #      "type": "service_account",
+   #     "project_id": os.environ.get("FIREBASE_PROJECT_ID"),
+    #    "private_key_id": os.environ.get("FIREBASE_PRIVATE_KEY_ID"),
+     #   "private_key": os.environ.get("FIREBASE_PRIVATE_KEY").replace('\\n', '\n'),
+      #  "client_email": os.environ.get("FIREBASE_CLIENT_EMAIL"),
+       # "client_id": os.environ.get("FIREBASE_CLIENT_ID"),
+        #"auth_uri": "https://accounts.google.com/o/oauth2/auth",
+       # "token_uri": "https://oauth2.googleapis.com/token",
+        #"auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
+        #"client_x509_cert_url": os.environ.get("FIREBASE_CLIENT_CERT_URL")
+  #  }
     
-    cred = credentials.Certificate(cred_dict)
-    firebase_admin.initialize_app(cred, {
-        'databaseURL': os.environ.get("FIREBASE_DATABASE_URL")
-    })
+    #cred = credentials.Certificate(cred_dict)
+    #firebase_admin.initialize_app(cred, {
+     #   'databaseURL': os.environ.get("FIREBASE_DATABASE_URL")
+    #})
 def registrar_palpite(nick, grupo, jogo_id, t1, t2, g1, g2):
     # ... seu código de salvamento ...
     db.child("palpites").child(nick).child(grupo).child(jogo_id).set({
